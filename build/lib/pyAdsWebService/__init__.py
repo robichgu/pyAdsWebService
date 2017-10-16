c246d4dd286fb06253dd6a6e3e53698f1e260517
Guillaume <guillaume@emeraldtherapeutics.com>
2017-10-15 17:00:32 -0700

    Deleted unwanted files

--- build/lib/pyAdsWebService/__init__.py
+++
@@ -1,6 +0,0 @@
-#!-*- coding: utf-8 -*-
-# from .pyAdsWebService import *
-
-
-from .pyAdsWebService import getvariablehandles, ReadADSWeb, WriteADSWeb, errorCode, \
-    varbyte, byteArrayToValue, valueToByteArray, flatten
